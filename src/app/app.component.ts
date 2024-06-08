7cde9c4f221653a22e272b8720e4b3530efc103f
CodeRacon <michael.buschmann.86@gmail.com>
2024-06-08 17:32:13 +0200

    feat: generated + implemented new component: 'start-screen'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
+// MD Imports
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+// Application Imports
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { StartScreenComponent } from './start-screen/start-screen.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSlideToggleModule],
+  imports: [RouterOutlet, MatSlideToggleModule, StartScreenComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
