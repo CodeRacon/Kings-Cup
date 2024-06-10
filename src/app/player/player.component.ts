ac0503cfe4554574873dbe4ac2a6823d449ab591
CodeRacon <michael.buschmann.86@gmail.com>
2024-06-10 12:41:41 +0200

    feat: added logic to highlight the current active player; overall styling

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -11,4 +11,5 @@ import { Component, Input } from '@angular/core';
 export class PlayerComponent {
   @Input() name!: string;
   @Input() avatar!: string;
+  @Input() playerActive: boolean = false;
 }
